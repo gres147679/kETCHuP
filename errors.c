9e1ee55b17a8485e29381feba8e82606d0043312
Gustavo EL Khoury <gustavoelkhoury@gmail.com>
2013-11-10 10:56:27 -0430

    Manejo de errores sencillo

---
+++ errors.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "errors.h"
+
+char *programname;
+
+void fatalerror(char *message) {
+  fprintf(stderr, "%s: %s\n", programname, message);
+  perror(programname);
+  fflush(stdout);
+  exit(1);
+}
+
+void errormessage(char *message) {
+  fprintf(stderr, "%s: %s\n", programname, message);
+  fflush(stdout);
+}
+
---
+++ errors.h
@@ -0,0 +1,10 @@
+#ifndef _ERRORS_H_
+#define _ERROR_H_
+
+extern char *programname;
+
+extern void fatalerror(char *message);
+extern void errormessage(char *message);
+
+#endif
+
