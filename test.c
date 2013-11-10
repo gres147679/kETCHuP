4b7c3b71c57baed4bc9bbfe0fa083fde16fd4ae1
Gustavo EL Khoury <gustavoelkhoury@gmail.com>
2013-11-10 11:21:50 -0430

    Pruebas robustas sobre la lista

--- test.c
+++ test.c
@@ -7,6 +7,20 @@ int main(){
 	addUser(&blah,"Jajaj2");
 	addUser(&blah,"Jajaj3");
 	addUser(&blah,"Jajaj4");
-	printLista(blah);
+	addUser(&blah,"Jajaj5");
+	printList(blah);
+
+	removeUser(&blah,"Jajaj1");
+	printf("Luego de borrar 1\n");
+	printList(blah);
+
+	removeUser(&blah,"Jajaj5");
+	printf("Luego de borrar 5\n");
+	printList(blah);
+
+	removeUser(&blah,"Jajaj3");
+	printf("Luego de borrar 3\n");
+	printList(blah);
+	
 	return 0;	
 }
