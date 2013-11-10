69069eb78037ec924dea1534b5e0a210239519a3
Gustavo EL Khoury <gustavoelkhoury@gmail.com>
2013-11-10 08:40:07 -0430

    Agregado el código del servidor

---
+++ schat.c
@@ -0,0 +1,52 @@
+/****************************
+ schat.c
+ 
+ Andrea Balbás        09-10076
+ Gustavo El Khoury    10-10226     
+ 
+ Septiembre - Diciembre 2013
+ ****************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+// AQUI VAN OTROS INCLUDE, NO ESTOY SEGURA CUALES
+#include <strings.h>
+#include "errors.h"
+
+#define port 20226
+
+main(int argc, char *argv[]){
+  int sockfd, newsockfd;  
+  struct sockaddr_in clientaddr,serveraddr;
+  int clientaddrlength;
+  //int pid;      LO PONGO COMENTADO PORQUE NO SE SI USAREMOS HILOS O PROCESOS
+  
+  /*Abrir el socket*/
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    fatalerror("No se puede abrir el socket");
+  
+  /*Asociar el socket a una direccion*/
+  bzero(&serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = PF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons(PORT);
+  if (bind(sockfd, (struct sockaddr *) &serveraddr,
+           sizeof(serveraddr)) != 0)
+    fatalerror("No se puede asociar a un socket");
+  if (listen(sockfd, QUEUELENGTH) < 0)
+    fatalerror("No se puede escuchar");
+    
+  /*Servidor*/
+  while (1) {
+    /* Wait for a connection. */
+    clientaddrlength = sizeof(clientaddr);
+    newsockfd = accept(sockfd, 
+                       (struct sockaddr *) &clientaddr,
+                       &clientaddrlength);
+  /* IMPLEMENTACION LOCA =) */
+  }
+  
+}
